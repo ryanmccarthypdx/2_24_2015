f65776049d25ceaaf763dd47f0c1dc67316f7727
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-02-24 15:03:47 -0800

    add pizza check total through checks factory

--- restaurant/controllers/PizzaController.js
+++ restaurant/controllers/PizzaController.js
@@ -1,8 +1,15 @@
 restaurant.controller('PizzaCtrl', function PizzaCtrl($scope, ChecksFactory) {
+  $scope.ChecksFactory = ChecksFactory;
   $scope.pizzaOptions = [
     {size: "small", price: 8},
     {size: "medium", price: 10},
     {size: "large", price: 12}
   ];
-  
+  $scope.pizzas = [];
+
+  $scope.addPizza = function(pizza) {
+    var newPizza = { size: pizza.size, price: pizza.price }
+    $scope.pizzas.push(newPizza);
+  }
+  $scope.pizza = $scope.pizzaOptions[2];
 });
--- restaurant/services/ChecksFactory.js
+++ restaurant/services/ChecksFactory.js
@@ -1,5 +1,12 @@
 restaurant.factory('ChecksFactory', function ChecksFactory() {
   var factory = {};
 
+  factory.total = function(items) {
+    var total = 0;
+    items.forEach(function(item) {
+      total += item.price;
+    });
+    return total.toFixed(2);
+  }
   return factory;
 });
