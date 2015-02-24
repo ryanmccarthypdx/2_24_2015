aaf7b30eb2c0be48599a0da250a65474a3dfbf2b
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-02-24 15:13:08 -0800

    copy pizza functionality over to ice cream using the same factory

--- restaurant/controllers/IceCreamController.js
+++ restaurant/controllers/IceCreamController.js
@@ -1,3 +1,15 @@
 restaurant.controller("IceCreamCtrl", function IceCreamCtrl($scope, ChecksFactory) {
+  $scope.ChecksFactory = ChecksFactory;
+  $scope.iceCreamOptions = [
+    {scoops: "One", price: 2},
+    {scoops: "Two", price: 3},
+    {scoops: "Three", price: 4}
+  ];
+  $scope.iceCreams = [];
 
+  $scope.addIceCream = function(iceCream) {
+    var newIceCream = { scoops: iceCream.scoops, price: iceCream.price }
+    $scope.iceCreams.push(newIceCream);
+  }
+  $scope.iceCream = $scope.iceCreamOptions[2];
 });
